7af0a2c8f5eeccb7d41ccb4916bfb08b59072dd8
Vladimir <timos9vs@gmail.com>
2019-07-03 17:14:19 +0300

    base bad solution

--- index.rb
+++ index.rb
@@ -1,17 +1,29 @@
+puts 'Введите имя файла'
+file_name = gets.chomp
+
 list_team = {}
 
-fil_rd = File.open('in.txt')
+fil_rd = File.open(file_name)
 
 fil_rd.each do |line|
-  tmp = line.split(',')
-  tmp.each do |row|
-    row.strip!
-    if row
-      tmp = row.split(' ')
-      res = tmp.pop.to_i
-      list_team.key?(tmp.join) ? list_team[tmp.join] += res : list_team[tmp.join] = res
-    end
+  line.strip!
+  tmp = line.split(',') if line
+  tmp1 = tmp[0].split(' ')
+  tmp2 = tmp[1].split(' ')
+  res1 = tmp1.pop.to_i
+  res2 = tmp2.pop.to_i
+  case res1 <=> res2
+  when -1
+    list_team.key?(tmp1.join) ? list_team[tmp1.join] += 0 : list_team[tmp1.join] = 0
+    list_team.key?(tmp2.join) ? list_team[tmp2.join] += 3 : list_team[tmp2.join] = 3
+  when 0
+    list_team.key?(tmp1.join) ? list_team[tmp1.join] += 1 : list_team[tmp1.join] = 1
+    list_team.key?(tmp2.join) ? list_team[tmp2.join] += 1 : list_team[tmp2.join] = 1
+  when 1
+    list_team.key?(tmp1.join) ? list_team[tmp1.join] += 3 : list_team[tmp1.join] = 3
+    list_team.key?(tmp2.join) ? list_team[tmp2.join] += 0 : list_team[tmp2.join] = 0
   end
+
 end
 list_team = list_team.sort_by{|k, v| [-v, k]}
 index = 0
