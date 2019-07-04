61925a006132628544b5c096a9a5f23a76b7a6c6
Vladimir <timos9vs@gmail.com>
2019-07-04 11:41:42 +0300

    add module for run

--- index.rb
+++ index.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
-require './soccer_ranking'
+# run preparing file
+module RunModule
+  require './soccer_ranking'
+  def self.run
+    sm = SoccerModule::SoccerRanking.new
+    ARGV.size.positive? ? sm.file_name = ARGV[0] : sm.filename
+    sm.read_file
+    sm.print_table
+  end
+end
 
-sm = SoccerModule::SoccerRanking.new
-ARGV.size.positive? ? sm.file_name = ARGV[0] : sm.filename
-sm.read_file
-sm.print_table
+RunModule.run
